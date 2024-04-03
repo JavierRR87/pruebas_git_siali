701d130503ba91a1888ba4265615b00ea43e70d7
JavierRR <ntecnologiasjrr@gmail.com>
2024-04-03 12:36:47 +0200

    Funcion restar

---
+++ restar.py
@@ -0,0 +1,7 @@
+## la funcion pasa a ser restar 2 numeros.
+def restar_dos_numeros(numero1, numero2):
+    return numero1 - numero2
+
+# Ejemplo de uso
+resultado = sumar_dos_numeros(5, 3)
+print(resultado)  # Esto imprimirá 2
